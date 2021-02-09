bff479d6c8645842ae97000d44e2a512b5354251
Kieran <kieranherr@gmail.com>
2021-02-09 15:49:07 -0600

    init

---
+++ LittleFishEnt/LittleFishEnt/Models/Bios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LittleFishEnt.Models
+{
+    public class Bios
+    {
+        [Key]
+        public int BioId { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Type { get; set; }
+
+    }
+}
---
+++ LittleFishEnt/LittleFishEnt/Models/Images.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LittleFishEnt.Models
+{
+    public class Images
+    {
+       [Key]
+        public int ImageId { get; set; }
+        public string ImageName { get; set; }
+        public string ImageLocation { get; set; }
+        
+    }
+
+}
